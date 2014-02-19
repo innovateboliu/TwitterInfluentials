bc6d11c632b01dd4507477b5e162ae58868d4ac4
Bo Liu <wy90021@gmail.com>
2014-02-19 13:26:21 -0800

    side distribution cache

--- src/main/java/com/bo/GraphContructionMapper.java
+++ src/main/java/com/bo/GraphContructionMapper.java
@@ -2,12 +2,15 @@ package com.bo;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -28,10 +31,11 @@ public class GraphContructionMapper extends Mapper<LongWritable, Text, Text, Tex
 	}
 	
 	private void buildCache(URI[] cacheFileUris) throws IOException {
+		FileSystem fs = FileSystem.get(new Configuration());
 		for (URI uri : cacheFileUris) {
 			BufferedReader reader = null;
 			try {
-				reader = new BufferedReader(new FileReader(uri.toString()));
+				reader = new BufferedReader(new InputStreamReader(fs.open(new Path(uri))));
 				String line;
 				while ((line = reader.readLine()) != null) {
 					System.out.println("!!!!!!!!!!!!!!!!!!!! line is "+line);
