1d409ebf4761eda9ee8c55e73b4823bb8c09dae9
Bo Liu <wy90021@gmail.com>
2014-02-25 19:56:44 -0800

    test pagerank

--- src/main/java/ranking/pagerank/PageRankMapper.java
+++ src/main/java/ranking/pagerank/PageRankMapper.java
@@ -13,10 +13,15 @@ public class PageRankMapper extends Mapper<LongWritable, Text, Text, Text> {
 			throws IOException, InterruptedException {
 		String[] tokens = value.toString().split("\\s");
 		String[] tos = null;
+		System.out.println("tokens " + tokens);
 		
 		if (tokens.length == 3) {
 			tos = tokens[2].split(",");
+			System.out.println("tos " + tokens);
+
 		} else {
+			System.out.println("return due to tokens");
+
 			return;
 		}
 		
@@ -26,12 +31,18 @@ public class PageRankMapper extends Mapper<LongWritable, Text, Text, Text> {
 		int numTo = tos.length - 1;
 
 		if (numTo < 1) {
+			System.out.println("return due to numTo");
+
 			return;
 		}
 
 		float val = score / numTo;
 
 		for (int i = 1; i < tos.length; i++) {
+			System.out.println("to " + tos[i]);
+			System.out.println("to " + val);
+
+
 			context.write(new Text(tos[i]), new Text(Float.toString(val)));
 		}
 
